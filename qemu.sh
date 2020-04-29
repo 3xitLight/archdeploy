be5d161ad5eaa4b98cc974ccf4d4006a209a3a2a
3xitlight <3xitlight@gmail.com>
2020-04-29 20:01:49 +0200

    update

--- qemu.sh
+++ qemu.sh
@@ -12,8 +12,8 @@ qemu-system-x86_64 -machine type=pc,accel=kvm -m 4G -enable-kvm -cpu host -smp c
 ${uefi} \
 -drive file=/dev/sdb,if=virtio,format=raw \
 -drive file=${iso},media=cdrom -boot d \
--device usb-ehci,id=ehci -device usb-host,bus=ehci.0,hostbus=8,hostaddr=1 \
--device usb-ehci,id=ehci1 -device usb-host,bus=ehci.0,vendorid=0x2357,productid=0x010d \
+-device usb-ehci,id=ehci -device usb-host,bus=ehci.0,hostbus=7,hostaddr=7 \
+-device usb-ehci,id=ehci1 -device usb-host,bus=ehci.0,vendorid=0x0bda,productid=0x8153 \
 -usb -device usb-tablet \
 -vga std \
 -k de
